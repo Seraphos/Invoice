310482bbdd64a7b4e865de83614b4e239be5e272
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 15:01:33 -0400

    Add project files.

---
+++ Invoice/Invoice.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice
+{
+    class Invoice
+    {
+        private string partNumber, partDescription;
+        private decimal pricePerItem;
+        private int itemQuantity;
+
+        //constructor
+        public Invoice(string constructorPartDescription, int constructorItemQuantity, decimal constructorPricePerItem, string constructorPartNumber)
+        {
+            partDescription = constructorPartDescription;
+            itemQuantity = constructorItemQuantity;
+            pricePerItem = constructorPricePerItem;
+            partNumber = constructorPartNumber;
+        }//end constructor
+
+
+        public string PartNumber
+        {
+            get { return partNumber; }//end part number get
+            set { partNumber = value; }//end part number set
+        }//end part number property
+        public string PartDescription
+        {
+            get { return partDescription; }//end partDesc Property
+            set { partDescription = value; }//end partDesc Property 
+        }//end PartDesc property
+        public decimal PricePerItem
+        {
+            get { return pricePerItem; }//end PPI get
+            set
+            {
+                if (pricePerItem > 0) pricePerItem = value;
+                if (pricePerItem <= 0) throw new ArgumentOutOfRangeException("Only non-zero positive numbers are allowed.");
+            }//end PPI set
+        }//end PPI property
+        public int ItemQuantity
+        {
+            get { return itemQuantity; }//end IQ get
+            set
+            {
+                if (itemQuantity > 0) itemQuantity = value;
+                if (itemQuantity <= 0) throw new ArgumentOutOfRangeException("Only non-zero positive numbers are allowed.");
+            }//end IQ set
+        }//end IQ property
+
+        public decimal GetInvoiceAmount(decimal pricePerItem, int itemQuantity)
+        {
+            decimal product;
+            product = pricePerItem * Convert.ToDecimal(itemQuantity);
+            return product;
+        }//end get invoice amount method
+
+
+    }//end class
+}//end namespace
---
+++ Invoice/InvoiceTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice
+{
+    class InvoiceTest
+    {
+        static void Main(string[] args)
+        {
+
+            decimal pricePerItem, invoiceAmount;
+            int quantity;
+
+            Console.WriteLine("Please enter the price per item: ");
+            pricePerItem = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter the quantity requested by the customer: ");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+
+            //instantiate invoice object
+            Invoice myInvoice = new Invoice("Super Charged Cog", quantity, pricePerItem, "4H12345");
+            //use get invoice amount method
+            invoiceAmount = myInvoice.GetInvoiceAmount(pricePerItem, quantity);
+            Console.WriteLine("The description is: {0}\nThe number ordered is: {1}\nThe price per item is: {2:C}\nThe item part number is: {3}",
+                myInvoice.PartDescription, myInvoice.ItemQuantity, myInvoice.PricePerItem, myInvoice.PartNumber);
+            Console.WriteLine("The total invoice amount is: {0:C}", invoiceAmount);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
